473cb0660bc640a8749ab410d833b50d494df090
fourwheel2005 <kasidit9911@gmail.com>
2025-05-20 11:11:46 +0700

    Add files via upload

---
+++ src/main/java/com/example/linechatbot/LinechatbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.linechatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinechatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinechatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/linechatbot/LinechatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linechatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinechatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
