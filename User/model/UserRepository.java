e50b53ea786e34949a8eb401edbec19f56c8259b
fourwheel2005 <kasidit9911@gmail.com>
2025-05-20 23:04:20 +0700

    fix : repository

--- User/model/UserRepository.java
+++ User/model/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long>
 {
     User findByName(String name);
