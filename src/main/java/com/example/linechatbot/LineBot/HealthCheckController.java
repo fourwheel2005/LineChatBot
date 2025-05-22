b44e9fe867b2b870fb97211293cdd75c2056a5d7
fourwheel2005 <kasidit9911@gmail.com>
2025-05-22 13:10:59 +0700

    feat : healthcheck api

---
+++ src/main/java/com/example/linechatbot/LineBot/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.example.linechatbot.LineBot;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "OK";
+    }
+}
+
